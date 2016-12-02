579a11ecc07434e2231be9ec456e48dae351449e
Jake Kara <jake@jakekara.com>
2016-12-02 15:17:04 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,56 @@
+# Exploring official CT election data
+
+The Secretary of the State's office published complete "real-time" election
+data for the first time during the 2016 presidential election
+(here)[http://ctemspublic.pcctg.net/#/home]. I air-quote "real-time" rather
+than leave it out altogether, because the data was available as fast as the
+SOTS and town clerks were able to get it into the system, but it wasn't all
+available on election night. 100% of precincts were not reporting data
+until a week after the election, and there were recounts required in state
+legislature races. The results in the system were not marked "official"
+until December. Still it was "good 'nuff for government work."
+
+# total statewide vote results
+
+The /output/merged_state_votes.csv file contains results (vote counts and
+percents), candidate, office and party information for every race on every
+ballot in every town in Connecticut for the Nov. 8 election. (It also
+includes write-in candidates who were not on the ballot...).
+
+Please feel free to use this for your own analysis. I'd be happy to hear
+from you if you do, by emailing me at jkara@trendct.org, but it's not
+required.
+
+If you do any analysis, note that many candidates cross-endorsed. In those
+cases, they appear in the table as separate candidates. You would need to
+combine them to calculate accurate vote totals for a specific
+individual. There were no cross endorsements in the presidential race.
+
+If you want to parse out data that I haven't included in the spreadsheet
+but which the SOTS' raw data includes, read on.
+
+# /data/
+
+There are two files that serve the state's election results interface, both
+of which are in the /data/ foler of this repository:
+
+* Electiondata_final.json
+* Lookupdata_final.json
+
+(the "_final" was added by me to denote the version of the file I
+downloaded once the values were certified).
+
+# parsing
+
+I used these data files extensively to update data pages on (the election
+blog and data portal I built for
+TrendCT.org)[https://blogotron.ctmirror.org/election-2016/].
+
+It includes information on polling places, town an state election results,
+lookup objects candidate names and parties.
+
+The file election-sheets.py demonstrates how I accomplished some of the
+parsing to populate these graphics. The product of this script are stored
+in the /output/ foler of this repository, most notably in the file
+"merged_state_votes.csv".
+
---
+++ election-sheets.py
@@ -0,0 +1,113 @@
+# Hacking CT Election data with python
+# by Jake Kara
+
+import json, pandas as pd, numpy  as np
+from dateutil.parser import parse
+
+class CTElection:
+    # read data files, final, official data from CT SOTS website
+
+    def __init__(self, edata_filename="data/Electiondata_final.json",
+                 ldata_filename="data/Lookupdata_final.json"):
+        
+        # edata - election data; this contains results data
+        self.edata = json.loads(open(edata_filename).read())
+
+        # ldata - lookup data; this contains meta data and information on
+        # non-results, such as canidate names and polling places
+        self.ldata = json.loads(open(ldata_filename).read())
+
+    # Return the name of the election
+    def name(self):
+        return self.ldata["election"]["DNM"]
+
+    # Return the date of the election as a date, not a string
+    def date(self):
+        return parse(self.ldata["election"]["DT"])
+
+    def unweird(self, obj, fieldName="weird"):
+        # many arrays in the election system contain a weird structure. Here's an example with ldata["officeList"]
+        # the objects have one index, which is the id of the office.
+        # we need to get that id to access its value, and the data in there is what we want.
+        # Yep, it's weird. Why not just a dictionary with these as keys? I don't know.
+        # Here's an example 
+        # [{...},...,{u'306': {u'OO': u'223', u'D': u'0', u'DT': u'', u'OT': u'SM', u'ID': u'306', u'NM': u'Registrar of Voters - Danbury'}}]
+
+        k = obj.keys()[0]
+
+        ret = obj[k]
+        ret[fieldName] = k
+        return ret
+    
+    # Return a list of offices
+    def offices(self):
+        ret = []
+        arr = self.ldata["officeList"]
+        for obj in arr:
+            ret.append(self.unweird(obj))
+        return pd.read_json(json.dumps(ret)).set_index("ID")
+
+    def stateVotes(self):
+        votes = self.edata["stateVotes"]
+        ret = []
+
+        for k in votes.keys():
+            obj = votes[k]
+            # obj contains an object for each candidate
+            for cand in obj:
+                candResult = self.unweird(cand, "candId")
+                candResult["officeId"] = k
+                ret.append(candResult)
+                
+        return pd.read_json(json.dumps(ret)).set_index("candId")
+
+    def parties(self):
+        parties =  self.ldata["partyIds"]
+        partyLookup = {}
+        ret = []
+        
+        for pId in parties.keys():
+            obj = parties[pId]
+            obj["partyId"] = pId
+            ret.append(obj)
+
+        return pd.read_json(json.dumps(ret)).set_index("partyId")
+    
+    def candidates(self):
+        cands = self.ldata["candidateIds"]
+        ret = []
+        for candId in cands.keys():
+            obj = cands[candId]
+            obj["candId"] = candId
+            ret.append(obj)
+        ret = pd.read_json(json.dumps(ret))#.set_index(candId)
+
+        ret.set_index("candId")
+        ps = self.parties()
+        ret = ret.join(ps, on="P", how="left", lsuffix="_CAND", rsuffix="_PARTY")
+        ret.set_index("candId")
+        return ret
+
+# Generating some reports...
+
+e = CTElection()    
+
+print "Parsing data for election '" + e.name() + "' which occurred on " + str(e.date())
+
+print "Generating output/office_list.csv..."
+e.offices().to_csv("output/office_list.csv",index=False)
+
+print "Generating output/state_votes.csv..."
+e.stateVotes().sort_index().to_csv("output/state_votes.csv")
+
+print "Generating output/candidate_names.csv..."
+e.candidates().sort_values(by="candId").to_csv("output/candidate_names.csv",index=False)
+
+print "Generating output/merged_state_votes.csv..."
+
+e.stateVotes()\
+ .join(e.candidates().set_index("candId"),how="left")\
+ .set_index("officeId")\
+ .join(e.offices(),how="left", rsuffix="_OFFICE")\
+ .sort_index().to_csv("output/merged_state_votes.csv",
+                      index=False)
